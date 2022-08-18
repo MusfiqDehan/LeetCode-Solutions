af12345861bcb89e5dd2169e024f3f0fe8591c72
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-08-18 09:38:46 +0600

    Time: 52 ms (37.57%), Space: 14 MB (10.11%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -1,6 +1,21 @@
-import numpy as np
+# import numpy as np
+
+# class Solution:
+#     def subtractProductAndSum(self, n: int) -> int:
+#         a = [int(x) for x in str(n)]
+#         return np.prod(a) - np.sum(a)
+
+
 
 class Solution:
     def subtractProductAndSum(self, n: int) -> int:
-        a = [int(x) for x in str(n)]
-        return np.prod(a) - np.sum(a)
+        summation = 0
+        multiplication = 1
+        if not n:
+            return 0
+        while n:
+            digit = n % 10
+            n = n // 10
+            summation += digit
+            multiplication *= digit
+        return multiplication-summation
