ebac38676a426f97237405ad4f46ec0ca7ccc3a7
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-08-18 09:00:28 +0600

    Time: 36 ms (85.50%), Space: 13.9 MB (54.81%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -1,12 +1,28 @@
+# class Solution:
+#     def average(self, salary: List[int]) -> float:
+#         min_salary = min(salary)        
+#         max_salary = max(salary)
+#         len_counted = len(salary) - 2
+#         total_salary = sum(salary)
+#         remaining_salary = total_salary - (min_salary + max_salary)
+#         avg_salary = remaining_salary / len_counted
+#         return avg_salary
+
 class Solution:
     def average(self, salary: List[int]) -> float:
-        min_salary = min(salary)        
-        max_salary = max(salary)
-        len_counted = len(salary) - 2
-        total_salary = sum(salary)
-        remaining_salary = total_salary - (min_salary + max_salary)
-        avg_salary = remaining_salary / len_counted
-        return avg_salary
+        nmin, nmax = float('inf'), float('-inf')
+        nsum = count = 0
+        for val in salary:
+            if val < nmin:
+                nmin = val
+            if val > nmax:
+                nmax = val
+            nsum = nsum + val
+            count = count + 1
+            
+        avg = (nsum - (nmax + nmin)) / (count - 2)
+        
+        return avg
 
         
         
