7ea70d2504b97b1f8bf23917de25bffb86ad5258
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-08-24 21:52:59 +0600

    Time: 58 ms (21.48%), Space: 13.9 MB (55.84%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,4 +1,9 @@
+# class Solution:
+#     def interpret(self, command: str) -> str:
+#         return command.replace('()','o').replace('(al)','al')
+
+import re
 class Solution:
     def interpret(self, command: str) -> str:
-        return command.replace('()','o').replace('(al)','al')
+        return re.sub('\(\)', 'o', re.sub('\(al\)', 'al', command))
         
