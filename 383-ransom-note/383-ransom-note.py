ee0f2a5da5478a97855bd182502d3f2317a09737
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-08-25 11:28:51 +0600

    Time: 50 ms (94.85%), Space: 14.1 MB (53.79%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        for i in set(ransomNote):
+            if ransomNote.count(i) > magazine.count(i):
+                return False
+        return True
+        
