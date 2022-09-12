f8d3c9392bab2db82785d92071295ca923122b1f
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-09-12 12:36:06 +0600

    Time: 670 ms (20.65%), Space: 14.2 MB (97.15%) - LeetHub

---
+++ 676-implement-magic-dictionary/676-implement-magic-dictionary.py
@@ -0,0 +1,18 @@
+class MagicDictionary(object):
+    def __init__(self):
+        self.buckets = collections.defaultdict(list)
+
+    def buildDict(self, words):
+        for word in words:
+            self.buckets[len(word)].append(word)
+
+    def search(self, word):
+        return any(sum(a!=b for a,b in zip(word, candidate)) == 1
+                   for candidate in self.buckets[len(word)])
+        
+
+
+# Your MagicDictionary object will be instantiated and called as such:
+# obj = MagicDictionary()
+# obj.buildDict(dictionary)
+# param_2 = obj.search(searchWord)
