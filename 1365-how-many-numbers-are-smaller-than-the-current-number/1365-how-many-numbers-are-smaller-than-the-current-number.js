41424a38707eccc0eb9c140103e2205550b0a3c8
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-09-13 12:40:50 +0600

    Time: 150 ms (16.86%), Space: 43.6 MB (97.02%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -3,23 +3,24 @@
  * @return {number[]}
  */
 
-var smallerNumbersThanCurrent = function(nums) {
-     let result = [];
+var smallerNumbersThanCurrent = function(nums) {  
+    let result = [];
     
-     for(let i = 0 ; i < nums.length; i++){
-         let count = 0;
-         let j=0;
-         
-         while(j < nums.length) {
-             if(nums[i] > nums[j]) {
-                 count++;
-                 j++;
-             }
-             else {
-                 j++;
-             }
-         }
-         result.push(count);
-     }
-     return result;
+    for(let i = 0; i < nums.length; i++) {
+        let count = 0;
+        let j = 0;
+        
+        while(j < nums.length) {
+            if(nums[i] > nums[j]) {
+                count++;
+                j++;
+            }
+            else {
+                j++;
+            }
+        }
+        result.push(count);
+    }
+    return result
+    
 };
