946be3b3b17654099feff180370bf8f85fc2797b
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-09-27 16:16:01 +0600

    Time: 90 ms (60.25%), Space: 15.1 MB (38.01%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
@@ -10,8 +10,11 @@ class Solution:
         while i < j:
             while i < j and s[i] not in vows: i += 1
             while i < j and s[j] not in vows: j -= 1
+                
             if i > j: break
             s[i], s[j] = s[j], s[i]
+            
             i, j = i + 1, j - 1
+            
         return ''.join(s)
             
