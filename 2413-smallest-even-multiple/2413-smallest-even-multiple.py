6c3c59fb4727f7ab20dfaf68fa76ca7d6830ab22
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-09-28 15:47:01 +0600

    Time: 31 ms (94.48%), Space: 13.8 MB (51.06%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -0,0 +1,16 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 == 0:
+            return n
+        return n * 2
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
