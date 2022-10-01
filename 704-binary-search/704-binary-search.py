5cd2af95f3bba5145e0663e6b03a0d3305dcbd1f
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-10-01 12:17:35 +0600

    Time: 838 ms (5.00%), Space: 15.6 MB (26.54%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,7 +1,35 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        for i, v in enumerate(nums):
-            if v == target:
-                return i
+        # Brute Force
+        
+        # for i, v in enumerate(nums):
+        #     if v == target:
+        #         return i
+        # return -1
+    
+        # Binary Search
+        
+        l, r = 0, len(nums)-1
+        
+        while l <= r:
+            m = (l + r) // 2 
+            # m = (0+5) // 2 = 2.5 = 2
+            
+            if nums[m] > target: 
+                # nums[2] = 3 > 9
+                
+                r = m - 1
+            elif nums[m] < target:
+                # nums[2] = 3 < 9
+                l = m + 1 
+                # m = 2+1=3+1=4 
+                # nums[2]=3, nums[3]=5, nums[4]=9
+                
+            else:
+                # nums[m] == target
+                # 9 == 9
+                return m
+            
         return -1
+                
         
