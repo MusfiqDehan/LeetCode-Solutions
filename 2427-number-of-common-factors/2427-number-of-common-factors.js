4aed14ac7e1442d768ce662ead21c8bdf94342b5
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-10-02 22:41:26 +0600

    Time: 112 ms (100.00%), Space: 41.5 MB (100.00%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.js
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -5,9 +5,9 @@
  */
 var commonFactors = function(a, b) {
     let cnt = 0;
-    let n = ( Math.min(a, b) + 1 );
+    let n = (Math.min(a, b) + 1);
     
-    for ( let i = 0; i <= n; i++){
+    for (let i = 0; i <= n; i++){
         if (a % i === 0 && b % i === 0) {
             cnt++;
         }
