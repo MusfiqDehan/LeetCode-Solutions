b86aab8af183e8640a34d81c95601d362b9fc9f3
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-10-24 10:07:41 +0600

    Time: 187 ms (65.74%), Space: 52.1 MB (10.25%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        
+        unique = []
+        for s in arr:
+            u = set(s)
+            if len(u) == len(s): unique.append(u)
+        
+        concat = [set()]
+        for u in unique:
+            for c in concat:
+                if not c & u:
+                    concat.append(c | u)
+        
+        return max(len(cc) for cc in concat)
+        
