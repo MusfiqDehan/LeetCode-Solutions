06eeb4679c88188afdfe9612f6cbd71947887831
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-01-02 22:57:52 +0600

    Time: 28 ms (95.64%), Space: 13.8 MB (56.30%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,6 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word == word.lower() or word == word.upper() or word == word[0].upper()+word[1:].lower():
+            return True
+        else:
+            return False
