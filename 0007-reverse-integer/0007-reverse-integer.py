1a1955c064784e23b020beabc88600487249e528
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-01-29 23:13:48 +0600

    Time: 31 ms (88.82%), Space: 13.9 MB (12.40%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverse(self, x):
+        sign = 1
+        
+        if x < 0:
+            sign = -1
+            x *= -1
+            
+        x = int(str(x)[::-1])
+        
+        if x > 2**31-1 or x < 2**31 * -1:
+            return 0
+        
+        return sign * x
+       
+        
