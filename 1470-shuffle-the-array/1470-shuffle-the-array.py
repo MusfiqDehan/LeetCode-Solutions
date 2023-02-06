b93f842af691500e7a68d880b8f96a1b0b6e7b68
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-02-06 18:58:45 +0600

    Time: 56 ms (94.54%), Space: 14.1 MB (86.30%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        for i, j in zip(nums[:n],nums[n:]):
+            res += [i,j]
+        return res
+        
