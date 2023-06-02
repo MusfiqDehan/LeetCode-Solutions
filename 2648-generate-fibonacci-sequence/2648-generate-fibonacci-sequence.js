41936e5f4c5429e8e7ed85e832fe239c66faba37
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-06-02 10:37:23 +0600

    Time: 61 ms (38.46%), Space: 42.6 MB (14.14%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -0,0 +1,20 @@
+/**
+ * @return {Generator<number>}
+ */
+
+var fibGenerator = function*() {
+
+    let prev1 = 0, prev2 = 1;
+
+    while (true) {
+      yield prev1;
+      [prev1, prev2] = [prev2, prev1+prev2];
+    }
+
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
