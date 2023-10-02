32f9f6fbf1d38f26be88f76b80a0f67f22f5f98b
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2023-10-02 22:49:18 +0600

    Time: 198 ms (58.66%), Space: 17.3 MB (65.73%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -0,0 +1,11 @@
+class Solution:
+    def winnerOfGame(self, colors: str) -> bool:
+        alice, bob, = 0, 0
+        for i in range(len(colors) -2):
+            if colors[i] == colors[i +1] == colors[i +2]:
+                if colors[i] == 'A':
+                    alice += 1
+                else:
+                    bob += 1
+        return alice > bob
+        
