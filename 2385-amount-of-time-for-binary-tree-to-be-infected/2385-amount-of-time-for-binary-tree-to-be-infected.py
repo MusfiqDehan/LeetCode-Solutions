de0da4d035cbca4a8c44e2937b4c536141901ddc
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2024-01-10 23:55:12 +0600

    Time: 267 ms (99.44%), Space: 51.6 MB (98.51%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        result = 0
+
+        def DFS(node, start):
+            if node == None:
+                return 0
+
+            leftDepth = DFS(node.left, start)
+            rightDepth = DFS(node.right, start)
+
+            if node.val == start:
+                Solution.result = max(leftDepth, rightDepth)
+                return -1
+            
+            elif leftDepth >= 0 and rightDepth >= 0:
+                return max(leftDepth, rightDepth)+1
+            
+            Solution.result = max(Solution.result, abs(leftDepth - rightDepth))
+            return min(leftDepth, rightDepth) - 1
+
+        DFS(root, start)
+        return Solution.result
+        
